e57552769e7fff13a30dfe95343fd1d5283aa5b0
JoseRivero <josearmandorr95@gmail.com>
2022-05-30 17:30:41 +0200

    Initial commit

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/200.png"
+          className="App-logo"
+          alt="logo"
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
